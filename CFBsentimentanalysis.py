0df79b8abdfd2e857207cdadb25beab6be627b1c
WaelChettaoui <wael.chettaoui@medtech.tn>
2023-05-14 18:42:54 +0100

    Added bar graph and optimized output

--- CFBsentimentanalysis.py
+++ CFBsentimentanalysis.py
@@ -1,10 +1,10 @@
 import pandas as pd
 from textblob import TextBlob
+import matplotlib.pyplot as plt
+from io import BytesIO
 
 # Read the Excel file into a Pandas DataFrame
-data = pd.read_excel(r'C:\Users\waelc\OneDrive\Desktop\CFBsentimentanalysisproj\CS102_Input.xlsx')
-
-data
+data = pd.read_excel('CS102_Input.xlsx')
 
 # Create empty lists to store the sentiment scores
 polarity_scores = []
@@ -14,12 +14,12 @@ subjectivity_scores = []
 for index, row in data.iterrows():
     # Concatenate the text from multiple columns if needed
     text = ' '.join(str(row[column]) for column in data.columns)
-    
+
     # Create a TextBlob object and calculate sentiment scores
     blob = TextBlob(text)
     polarity = blob.sentiment.polarity
     subjectivity = blob.sentiment.subjectivity
-    
+
     # Append the scores to the respective lists
     polarity_scores.append(polarity)
     subjectivity_scores.append(subjectivity)
@@ -28,5 +28,33 @@ for index, row in data.iterrows():
 data['Polarity'] = polarity_scores
 data['Subjectivity'] = subjectivity_scores
 
-# Save the updated DataFrame to a new Excel file
-data.to_excel(r'C:\Users\waelc\OneDrive\Desktop\CFBsentimentanalysisproj\CS102_output.xlsx', index=False)
+# Create a bar graph of polarity scores
+plt.figure(figsize=(10, 6))
+plt.bar(range(len(polarity_scores)), polarity_scores)
+plt.xlabel('Row Index')
+plt.ylabel('Polarity Score')
+plt.title('Sentiment Analysis - Polarity Scores')
+plt.xticks(range(len(polarity_scores)), range(len(polarity_scores)), rotation='vertical', ha='center')
+plt.subplots_adjust(bottom=0.2)
+plt.grid(True)
+
+# Save the graph to a BytesIO object
+graph_buffer = BytesIO()
+plt.savefig(graph_buffer, format='png')
+plt.close()
+
+# Create an Excel writer object
+writer = pd.ExcelWriter('output_file.xlsx', engine='xlsxwriter')
+
+# Write the DataFrame to the Excel file on a new sheet
+data.to_excel(writer, index=False, sheet_name='Sentiment Analysis')
+
+# Create a new worksheet for the graph
+workbook = writer.book
+worksheet_graph = workbook.add_worksheet('Graph')
+
+# Insert the graph image into the worksheet
+worksheet_graph.insert_image('A1', 'graph.png', {'image_data': graph_buffer})
+
+# Save the Excel file
+writer.save()
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # CFBsentimentanalysis
 Basic python script for running sentiment analysis using textblob and pandas on spreadsheet data off local files. 
 
-Use an input .xlsx file by inputting its path in the body of the code, do the same with an empty .xlsx file. the script will add 2 new columns and fill them with the polarity and objectivity scores for each row composed of all the columns within the row concatenated with each other.
+Use an input .xlsx file by inputting its path in the body of the code, or placing the file itself in the same directory, in which case simply put the file's name instead of path. The script will generate a new file, give it a few seconds, and will add 2 new columns and fill them with the polarity and objectivity scores for each row composed of all the columns within the row concatenated with each other; as well as create a bar graph plotting each row's polarity score in a second sheet in the excel file.
